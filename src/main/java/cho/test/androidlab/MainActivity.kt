5c8e16208b847d18133a4fb08106979867112b43
chobkyu <qudqud97@naver.com>
2022-12-06 01:24:42 +0900

    ui 기초속성 및 실습

--- src/main/java/cho/test/androidlab/MainActivity.kt
+++ src/main/java/cho/test/androidlab/MainActivity.kt
@@ -1,43 +1,32 @@
 package cho.test.androidlab
 
-import android.graphics.Typeface
 import android.os.Bundle
-import android.view.Gravity
-import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.view.View
+import android.widget.Button
 import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var visibleButton: Button
+    lateinit var invisibleButton:Button
+    lateinit var logoImageView: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val name = TextView(this).apply{
-            typeface = Typeface.DEFAULT_BOLD
-            text="fuck"
-        }
-
-        val image = ImageView(this).also{
-            it.setImageDrawable(ContextCompat.getDrawable(
-                this,R.drawable.ic_launcher_background
-            ))
-        }
-
-        val title = TextView(this).apply{
-            typeface = Typeface.DEFAULT_BOLD
-            text = "hihihihihihi"
-        }
-
-        val layout = LinearLayout(this).apply{
-            orientation = LinearLayout.VERTICAL
-            gravity = Gravity.CENTER
-            addView(name, WRAP_CONTENT, WRAP_CONTENT)
-            addView(image, WRAP_CONTENT, WRAP_CONTENT)
-            addView(title, WRAP_CONTENT, WRAP_CONTENT)
-        }
-        //setContentView(layout)
         setContentView(R.layout.activity_main)
+
+        visibleButton = findViewById((R.id.visibleButton))
+        invisibleButton = findViewById(R.id.invisibleButton)
+        logoImageView = findViewById((R.id.logoImageView))
+
+        visibleButton.setOnClickListener{
+            logoImageView.visibility = View.VISIBLE
+        }
+
+        invisibleButton.setOnClickListener{
+            logoImageView.visibility = View.INVISIBLE
+        }
+
     }
 }
