4b198c232dedb742cd6178393be1cc19c2e75eb9
chobkyu <qudqud97@naver.com>
2022-12-12 23:54:11 +0900

    frame layout

--- src/main/java/cho/test/androidlab/MainActivity.kt
+++ src/main/java/cho/test/androidlab/MainActivity.kt
@@ -1,8 +1,10 @@
 package cho.test.androidlab
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +17,20 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_main)
 
+        val tab1 = findViewById<Button>(R.id.tab1)
+        val tab2 = findViewById<Button>(R.id.tab2)
+        val content1 = findViewById<TextView>(R.id.content1)
+        val content2 = findViewById<TextView>(R.id.content2)
+
+        tab1.setOnClickListener {
+            content1.visibility = View.VISIBLE
+            content2.visibility = View.INVISIBLE
+        }
+
+        tab2.setOnClickListener {
+            content2.visibility = View.VISIBLE
+            content1.visibility = View.INVISIBLE
+        }
 
 
     }
